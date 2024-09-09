d17452a5b3d2fdaebc3ab6b9430c061a5216baf4
norhanreda <Norhan.ahmed01@eng-st.cu.edu.eg>
2024-09-09 19:47:56 +0300

    adding parts initializations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import Header from "../components/header/header";
 import Footer from "../components/footer/footer";
+import Part1en from "../components/part1en/part1";
+import Part2en from "../components/part2en/part2en"
+import Part3en from "../components/part3en/part3en"
 
 export default function Home() {
   return (
     <>
     <Header/>
-    
+    <Part1en/>
+    <Part2en/>
+    <Part3en/>
     <Footer/>
     </>
   );
---
+++ src/components/part2en/part2en.tsx
@@ -0,0 +1,11 @@
+export default function Part1en() {
+    return (
+      <>
+        <h1 className="title">Part2</h1>
+        <p>
+        Implement a map using any library of your choice (Leaflet is recommended). Search for the coordinates of &apos;Digifly Company&apos; on Google Maps, then place a tooltip at these coordinates. Ensure that the tooltip styling matches the provided design below.
+        </p>
+      </>
+    );
+  }
+  
