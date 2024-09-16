5e4c2075d095916132ae2f6f5e6b059467dec0a3
norhanreda <Norhan.ahmed01@eng-st.cu.edu.eg>
2024-09-16 20:08:49 +0300

    remove unwanted code

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,10 +1,32 @@
+"use client";
+import { useSelector } from "react-redux";
 import "./footer.css"
+interface User {
+    id: number;
+    FirstName: string;
+    LastName: string;
+    Phone: string;
+    Email: string;
+  }
+  interface State {
+    users: User[];
+    language: string;
+    error: string | null;
+  }
 export default function Footer () {
+    const language:string = useSelector((state: State) => state.language); 
     return (
         <>
         <div className="footer">
             <footer>
-            <h3>All Rights Reseved &copy; Digifly 2024</h3>
+            {language ==="en" &&
+              <h3>All Rights Reseved &copy; Digifly 2024</h3>
+            }
+              {language ==="ar" &&
+              <h3>جميع الحقوق محفوظة &copy;ديجى فلاى 2024 </h3>
+            }
+           
+           
             </footer>
         </div>
         
